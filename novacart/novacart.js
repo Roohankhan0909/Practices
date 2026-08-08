e6ee02481c68872f5f19b3fe262beb67b0303b8d
Roohankhan0909 <132020500+Roohankhan0909@users.noreply.github.com>
2026-08-08 23:42:19 +0500

    cartbutton

--- novacart/novacart.js
+++ novacart/novacart.js
@@ -1,9 +1,16 @@
 const ShopNowBtn = document.querySelector("#shop-now-btn");
 const Heading = document.querySelector("#heroTitle");
 const paragraph = document.querySelector("#headinParagraph");
+const addToCartButton = document.querySelector("#jacket-cart-btn");
+const cartDisplay = document.querySelector("#cart-count");
 
 ShopNowBtn.addEventListener("click", () => {
   ((Heading.textContent = "Welcome to NovaCart!"),
     (paragraph.textContent = "Start exploring our amazing collection today!"),
     (ShopNowBtn.textContent = "Shopping..."));
+});
+let cartCount = 0;
+
+cart.addEventListener("click", () => {
+  cartCount = cartCount + 1;
 });
